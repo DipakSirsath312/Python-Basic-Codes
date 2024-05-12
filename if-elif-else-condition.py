65ca5ad960d750e0b313bdc52043008abe366156
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-05-12 22:54:16 +0530

    [ADDED]:Diapk - if-elif-else Condition Statement In Python

---
+++ if-elif-else-condition.py
@@ -0,0 +1,9 @@
+Trafficlight = input("Enter The Traffic Light Color:-")
+if(Trafficlight == "red"):
+    print("Stop")
+elif(Trafficlight == "yellow"):
+    print("Look")
+elif(Trafficlight == "green"):
+    print("Go")
+else:
+    print("Light is Broken")
