6ab4cd7ee22e96e7ec6e657ba4d4c85d3e380114
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-05-12 22:57:55 +0530

    [ADDED]:Dipak - Check The Student Grade Marks Condition Statement In Python

---
+++ if-elif-else-condition2.py
@@ -0,0 +1,9 @@
+Marks = int(input("Enter The Marks:-"))
+if(Marks >= 90):
+    print("A")
+elif(Marks >=80 and Marks < 90):
+    print("B")
+elif(Marks >=70 and Marks < 80):
+    print("C")
+else:
+    print("D")
