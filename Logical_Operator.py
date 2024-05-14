00abb59581ea5ece5c8591f7c9a3d93813574bc8
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-05-14 23:03:40 +0530

    [ADDED]:Dipak - The Logical Operator Operation In One Program

---
+++ Logical_Operator.py
@@ -0,0 +1,12 @@
+#Logical_Operator
+A = 50
+B = 30
+print(not (A > B))      #not operator
+print(not False)
+
+Val1 = True
+Val2 = False
+print("and operator:-",Val1 and Val2)   #and operator
+
+print("or operator:-",Val1 or Val2)     #or operator
+print("or operator:-",(A == B) or (A > B))
