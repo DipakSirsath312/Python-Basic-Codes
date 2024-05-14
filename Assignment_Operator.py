4e9b0b7624be3ffe4d5a4adf891675968a5842f2
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-05-14 21:11:52 +0530

    [ADDED]:Dipak - All Assignment Operators Operatiion In Codes

---
+++ Assignment_Operator.py
@@ -0,0 +1,19 @@
+#Assignment Operators
+num = 10
+num += 5
+print("Num :-",num)
+
+num -= 10
+print("Num :-",num)
+
+num *= 7
+print("Num :-",num)
+
+num /= 2
+print("Num :-",num)
+
+num %= 3
+print("Num :-",num)
+
+num **= 5
+print("Num :-",num)
