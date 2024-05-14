cf9d91a40146ba0e9bf41773464657e27fd07edc
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-05-14 21:06:05 +0530

    [ADDED]:Dipak - All Arithmeitc Operators Opration In Python

---
+++ Arithmetic_Operators.py
@@ -0,0 +1,9 @@
+#Arithmetic Operators
+Num1 = 10
+Num2 = 2
+print(Num1 + Num2)
+print(Num1 - Num2)
+print(Num1 * Num2)
+print(Num1 / Num2)
+print(Num1 % Num2)  #Remainder
+print(Num1 ** Num2) #Num1 ^ Num2
