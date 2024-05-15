c162a6ae5acbd222befc7fe9357c80940e33e363
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-05-15 20:58:37 +0530

    [ADDED]:Dipak - input statement is used to Accept values using keyboard from user

---
+++ Input-in-Python.py
@@ -0,0 +1,7 @@
+name = input("Enter The Name:- ")
+age = int(input("Enter Your Age:- "))
+marks = float(input("Enter a Marks:- "))
+
+print("Welcome",name)
+print("Age is",age)
+print("Your Marks",marks)
