a353af2ad6fec6040d558d937a3955814459de81
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-05-19 22:48:22 +0530

    [ADDED]:Dipak - Capitalize First Char in  String Function

---
+++ capitalize_stringfunction.py
@@ -0,0 +1,7 @@
+str = "dipak dinesh sirsath"
+print(str.capitalize()) #New String
+print(str)  #Old String
+
+str1 = "sirsath dipak dinesh"
+str1 = str1.capitalize() #This String is a New String and Modified.
+print(str1)
