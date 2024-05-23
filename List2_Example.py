d845e11a205388c586f1007f8aafb84659fcbe0b
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-05-23 21:11:58 +0530

    [ADDED]:Dipak - This Program Will Be List Data Type can Store Any Type Data

---
+++ List2_Example.py
@@ -0,0 +1,6 @@
+student = ["karan",95.4,19,"Delhi"]
+print(student)
+                            # String -> Inmutable 
+student[0] = "Arjun"        # Lists  -> Mutable
+student[3] = "Pune"
+print(student)
