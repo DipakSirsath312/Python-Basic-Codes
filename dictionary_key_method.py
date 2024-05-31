174802f2e4b31febd05b35c11260cd410aaf2ed1
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-05-31 22:03:15 +0530

    [ADDED]:Dipak - Dictionary Key Method Returns All Keys

---
+++ dictionary_key_method.py
@@ -0,0 +1,9 @@
+student = {
+    "name" : "dipak sirsath",
+    "roll no" : "359439",
+    "mobile number" : "8305783244",
+    "subject" : "python programing",
+}
+
+print(student.keys())
+print(list(student.keys())) #type casting in list
