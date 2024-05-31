29813d648506503e75ac39355eebaa45cda5dc04
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-05-31 22:05:46 +0530

    [ADDED]:Dipak - Dictionary Method Returns All Values

---
+++ dictionary_values_method.py
@@ -0,0 +1,9 @@
+student = {
+    "name" : "dipak sirsath",
+    "roll no" : "359439",
+    "mobile number" : "8305783244",
+    "address" : "khetiya",
+    "pin code" : "451881",
+}
+
+print(student.values())
