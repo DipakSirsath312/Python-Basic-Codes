8604591937510d37625ac9d05a43be9f9ab0f3f8
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-06-01 23:41:14 +0530

    [ADDED]:Dipak - inserts the specified items to the dictionary

---
+++ dictionary_update_method.py
@@ -0,0 +1,13 @@
+student = {
+    "name" : "dipak sirsath",
+    "subject": {
+        "phy" : 97,
+        "chem" : 98,
+        "math" : 95,
+    }
+}
+
+student.update({"city" : "delhi"})
+
+new_dict = {"name" : "alpesh sirsath","age" : 22} #update
+print(new_dict)
