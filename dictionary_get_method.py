61d030a6b46ddfb1624494c08894b7f8d783cb26
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-06-01 23:19:17 +0530

    [ADDED]:Dipak - returns the key according to value

---
+++ dictionary_get_method.py
@@ -0,0 +1,10 @@
+info = {
+    "name" : "Alpesh",
+    "subject" : ["python","java","c++"],
+    "Address" : "mp,khetiya",
+}
+
+print(info.get("subject"))
+
+print(info.get["name2"]) #error
+print(info.get("name2")) #no error -> none
