b127703036ed1fdcdea6b53b19deb5cfb96f8e40
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-06-03 21:22:40 +0530

    [ADDED]:Dipak - This Program will be set data type in python

---
+++ set_in_python.py
@@ -0,0 +1,8 @@
+#Set is the collection of the unordered items,
+#set must be unique and immutable.
+#Repeated elements stored only once.
+set_collection = {1,2,3,4,2,2,"hello","world","hello"} 
+
+print(set_collection)
+print(type(set_collection))
+print(len(set_collection))
