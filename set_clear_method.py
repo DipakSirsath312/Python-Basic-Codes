cc409e4dcc97f9964f70df9f56bc7f97bf9996ce
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-06-04 22:47:03 +0530

    [ADDED]:Dipak - empties the set

---
+++ set_clear_method.py
@@ -0,0 +1,9 @@
+set_collection = set()
+
+set_collection.add(1)
+set_collection.add(2)
+set_collection.add("ishwar")
+set_collection.add((1,2,3))
+
+set_collection.clear()
+print(len(set_collection))
