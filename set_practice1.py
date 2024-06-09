8f4607e32bfd9d58bfd8931c212b6718437cde9d
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-06-09 22:41:25 +0530

    [ADDED]:Dipak - way to store 9 and 9.0 as separate values in the set

---
+++ set_practice1.py
@@ -0,0 +1,10 @@
+#To store the 9 and 9.0 as seprate values in the set.
+#you can take help of built in data types.
+values = {9,"9.0"} 
+print(values)   # you can use the string data type.
+
+values = {
+    ("float",9.0),
+    ("int",9)
+}
+print(values)   # you can use tuple. 
