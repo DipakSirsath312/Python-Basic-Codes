a4c130c794ab732b481a1dfa0049cc1a7953e4eb
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-06-09 22:38:32 +0530

    [ADDED]:Dipak - store the subjects marks and use dictionary data type

---
+++ dictionary_practice3.py
@@ -0,0 +1,10 @@
+marks = {}
+x = int(input("Enter marks java:-"))
+marks.update({"java" : x})
+
+x = int(input("Enter marks Dsa:-"))
+marks.update({"Dsa" : x})
+
+x = int(input("Enter marks Webdev:-"))
+marks.update({"Webdev" : x})
+print(marks)
