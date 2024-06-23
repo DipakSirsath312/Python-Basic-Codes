f57598e73cdc468257fd24b13c3530d3cc9fd09f
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-06-23 21:53:38 +0530

    [ADDED]:Dipak - for loop traversing list,string,tuple

---
+++ for_loops_in_python.py
@@ -0,0 +1,14 @@
+#for loop in list
+list = [1,2,3,4,5,6,7,8,9,10]
+for nums in list:
+    print(nums)
+
+#for loop in string.
+veggies = ["potato","ladyfinger","brijal","potato"]
+for ele in veggies:
+    print(ele)
+    
+#for loop in tuple
+tup = (1,"ishwar",2,"yogesh",3,"chetan",4,"lalit")
+for values in tup:
+    print(values)
