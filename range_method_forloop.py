a404679209ccecb076199abae5c6442091f1a3b8
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-06-28 22:41:45 +0530

    [ADDED]:Dipak - range method in python

---
+++ range_method_forloop.py
@@ -0,0 +1,9 @@
+#range method syntax in for loop.
+for i in range(10): #range(stop)
+    print(i)
+
+for i in range(5,11): #range(start,stop)
+    print(i)
+
+for i in range(2,11,2): #range(start,stop,step)
+    print(i)
