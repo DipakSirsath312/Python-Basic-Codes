d79ccb64cb04305831258e13916b0924f2d66869
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-07-03 20:39:29 +0530

    [ADDED]:Dipak - function in python

---
+++ function_in_python.py
@@ -0,0 +1,9 @@
+#Function Defination.
+def cal_sum(a,b): #Function Parameters.
+    sum = a + b
+    print(sum)
+    return sum
+
+cal_sum(10,5) #Function call.
+cal_sum(20,35)
+cal_sum(100,500) #Arguments.
