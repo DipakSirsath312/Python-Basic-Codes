575fbc597d045c8b2080f6944b448b6628d93066
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-07-06 21:34:25 +0530

    [ADDED]:Dipak - function to print the evevn or odd numbers

---
+++ Function_Practice5.py
@@ -0,0 +1,18 @@
+#1
+num = int(input("Enter The Number ="))
+def Check_val(num):
+    if(num %2 == 0):
+        print("Even")
+    else:
+        print("Odd")
+
+Check_val(num)
+
+#2
+def Check_val(n):
+    if(n %2 == 0):
+        print("Even")
+    else:
+        print("Odd")
+
+Check_val(10)
