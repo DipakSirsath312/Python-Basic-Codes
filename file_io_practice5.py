a23ed2e4736b948c847e382c2cf013dc17f1ad1c
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-07-15 23:12:01 +0530

    [ADDED]:Dipak - file containing numbers separated by comma

---
+++ file_io_practice5.py
@@ -0,0 +1,10 @@
+count = 0
+with open("practice.txt1","r") as f:
+    data = f.read()
+
+    nums = data.split(",")
+    for val in nums:
+        if(int(val) %2 == 0):
+            count += 1
+
+print(count)
