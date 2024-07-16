fba22974456dac4b759965ec2159339a45760ba3
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-07-16 22:42:36 +0530

    [ADDED]:Dipak - class is a blueprint for creating objects

---
+++ class_object_demo.py
@@ -0,0 +1,15 @@
+class Student:
+    name = "Dipak"
+    Roll = 39
+    Id = 3766161
+
+s1 = Student()
+print(s1.name,s1.Roll,s1.Id)
+
+class Car:
+    Company = "TATA"
+    model = "safari"
+    color = "black"
+
+c1 = Car()
+print(c1.Company,c1.model,c1.color)
